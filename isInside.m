04a01ac80252629244e4bc6a3cb018d85bde7f14
peter-berthelemy <peter.berthelemy@gmail.com>
2023-06-28 17:04:08 +0100

    Added functions

---
+++ isInside.m
@@ -0,0 +1,10 @@
+function TF = isInside(ps1, ps2)
+    %Just for polyshapes I think, is ps1 inside ps2
+    if isempty(ps1.Vertices) == 1 || isempty(ps2.Vertices) == 1 
+        TF = 0;
+    else
+        TF = abs(area(ps2)-area(subtract(ps2,ps1))-area(ps1))<(area(ps1)*1e-06);
+%         disp(TF);
+    end
+end
+
